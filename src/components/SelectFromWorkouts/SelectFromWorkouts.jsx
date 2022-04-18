b9546ff544e2f22fb6743dde284e500419af3f30
Michael Chuinard <mpchuinard@finlandia.edu>
2022-04-18 13:48:02 -0500

    add workout name buttons

--- src/components/SelectFromWorkouts/SelectFromWorkouts.jsx
+++ src/components/SelectFromWorkouts/SelectFromWorkouts.jsx
@@ -10,6 +10,26 @@ function SelectFromWorkouts() {
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <p>Select From Workouts</p>
+      <button>Back Workout 1</button>
+      <button>Back Workout 2</button>
+      <button>Back Workout 3</button>
+      <button>Back Workout 4</button>
+      <button>Arm Workout 1</button>
+      <button>Arm Workout 2</button>
+      <button>Arm Workout 3</button>
+      <button>Arm Workout 4</button>
+      <button>Chest Workout 1</button>
+      <button>Chest Workout 2</button>
+      <button>Chest Workout 3</button>
+      <button>Chest Workout 4</button>
+      <button>Shoulder Workout 1</button>
+      <button>Shoulder Workout 2</button>
+      <button>Shoulder Workout 3</button>
+      <button>Shoulder Workout 4</button>
+      <button>Leg Workout 1</button>
+      <button>Leg Workout 2</button>
+      <button>Leg Workout 3</button>
+      <button>Leg Workout 4</button>
       <LogOutButton className="btn" />
     </div>
   );
