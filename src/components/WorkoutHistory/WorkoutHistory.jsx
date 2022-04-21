cf1ddebb185e0fea7580c766a967d939c885fae9
Michael Chuinard <mpchuinard@finlandia.edu>
2022-04-21 14:33:49 -0500

    add table data to workout history page

--- src/components/WorkoutHistory/WorkoutHistory.jsx
+++ src/components/WorkoutHistory/WorkoutHistory.jsx
@@ -19,22 +19,35 @@ function WorkoutHistory() {
     dispatch({ type: 'GET_WORKOUT_HISTORY' });
   }, []);
 
-  
-  return (
-    <section className="container">
-      
-      <div>{workoutHistory.map(lift => (
-        <h3>{lift.workout_name}</h3>
-          
 
-         
+  return (
+    
+    <section className="container">
+     
+
+      <main>{workoutHistory.map(lift => (
+        <>
+        <div>{lift.user_id}</div>
+        <div>{lift.workout_name}</div>
+        <div>{lift.exercise_1}</div>
+        <div>{lift.weight_used_1}</div>
+        <div>{lift.exercise_2}</div>
+        <div>{lift.weight_used_2}</div>
+        <div>{lift.exercise_3}</div>
+        <div>{lift.weight_used_3}</div>
+        <div>{lift.exercise_4}</div>
+        <div>{lift.weight_used_4}</div>
+        <div>{lift.date}</div>
+        </>
+        
+        
+        
+        
         
       ))}
-      </div>
-
+      </main>
     </section>
+    
   );
-
 }
-
 export default WorkoutHistory;
